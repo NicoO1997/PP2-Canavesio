205d0478db17c111d6a13baa2762ade73db67040
NicoO1997 <nicol4cho1997@gmail.com>
2024-08-24 11:01:35 -0300

    Agregar, Editar, Borrar maquinaria usada COMPLETO

--- pp3/canavesio/src/Controller/AdminUsedMachineryController.php
+++ pp3/canavesio/src/Controller/AdminUsedMachineryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\UsedMachinery;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,43 +22,48 @@ class AdminUsedMachineryController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/used-machinery/update', name: 'admin_update_used_machinery', methods: ['POST'])]
-    public function updateUsedMachinery(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/admin/used-machinery/update/{id}', name: 'admin_update_used_machinery', methods: ['POST'])]
+    public function updateUsedMachinery($id, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $machineryId = $request->request->get('machineryId');
-        $machineryData = $request->request->get('machineries')[$machineryId];
-
-        $machinery = $entityManager->getRepository(UsedMachinery::class)->find($machineryId);
-
-        if ($machinery) {
-            $machinery->setMachineryName($machineryData['machineryName']);
-            $machinery->setBrand($machineryData['brand']);
-            $machinery->setYearsOld(intval($machineryData['yearsOld']));
-            $machinery->setMonths(intval($machineryData['months'])); // Asegúrate de que esto esté presente
-            $machinery->setHoursOfUse(intval($machineryData['hoursOfUse']));
-            $machinery->setLastService(new \DateTime($machineryData['lastService']));
-            $machinery->setPrice($machineryData['price'] ? floatval($machineryData['price']) : null);
-
-            // Handling image upload
-            $imageFile = $request->files->get('machineries')[$machineryId]['image'];
-            if ($imageFile) {
-                $newFilename = uniqid().'.'.$imageFile->guessExtension();
-                $imageFile->move(
-                    $this->getParameter('kernel.project_dir').'/public/images',
-                    $newFilename
-                );
-                $machinery->setImageFilename($newFilename);
-            }
-
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Maquinaria actualizada con éxito.');
-        } else {
-            $this->addFlash('error', 'Maquinaria no encontrada.');
+        $machinery = $entityManager->getRepository(UsedMachinery::class)->find($id);
+    
+        if (!$machinery) {
+            throw $this->createNotFoundException('No machinery found for id ' . $id);
         }
-
+    
+        // Actualizar los datos del formulario
+        $machinery->setMachineryName($request->request->get('machineryName'));
+        $machinery->setBrand($request->request->get('brand'));
+        $machinery->setYearsOld($request->request->get('yearsOld'));
+        $machinery->setMonths($request->request->get('months'));
+        $machinery->setHoursOfUse($request->request->get('hoursOfUse'));
+        $machinery->setLastService(new \DateTime($request->request->get('lastService')));
+        $machinery->setPrice($request->request->get('price'));
+    
+        // Manejo de la imagen subida (solo si se ha subido una nueva imagen)
+        $image = $request->files->get('image');
+        if ($image) {
+            // Generar un nuevo nombre de archivo único y mover la imagen
+            $newFilename = uniqid() . '.' . $image->guessExtension();
+            $image->move($this->getParameter('images_directory'), $newFilename);
+    
+            // Borrar la imagen anterior si existe
+            if ($machinery->getImageFilename()) {
+                $oldImage = $this->getParameter('images_directory') . '/' . $machinery->getImageFilename();
+                if (file_exists($oldImage)) {
+                    unlink($oldImage);  // Eliminar la imagen anterior
+                }
+            }
+    
+            $machinery->setImageFilename($newFilename); // Guardar el nuevo nombre de imagen
+        }
+    
+        // Guardar los cambios
+        $entityManager->flush();
+    
+        // Redirigir nuevamente a la página de visualización
         return $this->redirectToRoute('admin_view_used_machinery');
-    }
+    }    
 
     #[Route('/used-machinery/delete/{id}', name: 'admin_delete_used_machinery')]
     public function deleteUsedMachinery(EntityManagerInterface $entityManager, UsedMachinery $usedMachinery): Response
